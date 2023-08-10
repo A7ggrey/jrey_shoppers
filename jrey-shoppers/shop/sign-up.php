23347cde66917c732e73844e61ba74b6ca0e7cc8
A7ggrey <84960729+A7ggrey@users.noreply.github.com>
2023-08-10 09:26:17 +0300

    Update sign-up.php

--- jrey-shoppers/shop/sign-up.php
+++ jrey-shoppers/shop/sign-up.php
@@ -2,9 +2,9 @@
 
 session_start();
     
-    require("./../includes/no-login-session.php");
+    //require("./../includes/no-login-session.php");
     require("./../includes/shoppers-database.php");
-    require("./../includes/session-messages.php");
+    //require("./../includes/session-messages.php");
 
 
     if (isset($_POST['btn-sign-up'])) {
